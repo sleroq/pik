c058d5d094fd1fccf408537e4dc21f63cdfb4f63
Sleroq <cantundo@pm.me>
2022-12-26 00:38:53 +0300

    First working version

--- src/components/Test/test.tsx
+++ src/components/Test/test.tsx
@@ -1,36 +1,45 @@
-import { component$, $ } from '@builder.io/qwik'
-import { WidgetApi } from 'matrix-widget-api'
+import { component$, useContext } from '@builder.io/qwik'
+import { MyContext, SharedState } from '~/root'
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const Test = component$(() => {
+  const state = useContext<SharedState>(MyContext)
+
   return (
     <>
-      <button onClick$={ $(whatever) } class="test-button">Test API</button>
-      <button onClick$={ $(sendSticker) } class="test-button">Send Sticker</button>
+      <button onClick$={() => {
+        if (!state.api) {
+          throw new Error('no api in state!')
+        }
+
+        console.log('sending sticker!')
+
+        const sticker = {
+          name: 'testing stickers!',
+          description: 'Isabella the Monero Girl glaring at the camera, cheeks red, with steam coming from her ears',
+          content: {
+              url: 'mxc://matrix.org/LLANaPGGqVzrvvQSWSxhSKRI',
+              info: {
+                  h: 256,
+                  w: 256,
+                  mimetype: 'image/png',
+                  size: 164934,
+              }
+          }
+        }
+        
+        state.api.sendSticker(sticker)
+      }} class="test-button">
+        Send Sticker
+      </button>
     </>
   )
 })
 
-export async function whatever () {
-  const sticker = {
-    name: 'meow',
-    content: {
-        url: 'https://scalar.vector.im/api/widgets/img/no-banana.png',
-        info: {
-            h: 120,
-            w: 120
-        }
-    }
-  }
-  const api = new WidgetApi()
+export async function sendSticker() {
+  // const api = new WidgetApi()
   console.log('meow meow')
-  console.log(await api.sendSticker(sticker))
-}
-
-export async function sendSticker () {
-    // const api = new WidgetApi()
-    console.log('meow meow')
-    // console.log(api.sendSticker)
+  // console.log(api.sendSticker)
 }
--- src/root.tsx
+++ src/root.tsx
@@ -1,12 +1,18 @@
-import { component$, useStyles$ } from '@builder.io/qwik'
+import { component$, useStyles$, createContext, useStore, useClientEffect$, useContextProvider, noSerialize, NoSerialize } from '@builder.io/qwik'
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
 } from '@builder.io/qwik-city'
 import { RouterHead } from './components/router-head/router-head'
+import { WidgetApi, MatrixCapabilities } from 'matrix-widget-api'
 
 import globalStyles from './global.css?inline'
+
+export interface SharedState {
+  api?: NoSerialize<WidgetApi>;
+}
+export const MyContext = createContext<SharedState>('everything');
 
 export default component$(() => {
   /**
@@ -17,13 +23,37 @@ export default component$(() => {
    */
   useStyles$(globalStyles)
 
+  const state = useStore<SharedState>({});
+
+  useClientEffect$(() => {
+    // TODO: Figure out how to get this
+    const widgetId = '!kzgAp0oSUS85E6NS%3Asleroq.link_%40sleroq%3Asleroq.link_1671992491687'
+    console.log('widget id: ', widgetId)
+
+    // TODO: Figure out how to get this
+    const api = new WidgetApi(widgetId, 'https://develop.element.io')
+    api.requestCapability(MatrixCapabilities.StickerSending)
+
+    api.on('ready', async function () {
+      console.log('widget is ready')
+    })
+
+    api.start()
+
+    // TODO: Is there a better way?
+    state.api = noSerialize(api)
+  })
+
+  useContextProvider(MyContext, state);
+
   return (
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
         <meta
           http-equiv="Content-Security-Policy"
-          content="frame-ancestors https://*;" />
+          content="frame-ancestors https://*;"
+        />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
