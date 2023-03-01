5cfc4217b078adf33916b73c9e87580a2c096d4f
Sleroq <sleroq@sleroq.link>
2023-03-02 00:20:53 +0300

    Added import from telegram feature to the bot

--- backpik/src/bot.ts
+++ backpik/src/bot.ts
@@ -1,41 +1,79 @@
 import {
-    MatrixClient,
-    SimpleFsStorageProvider,
-    AutojoinRoomsMixin,
+  MatrixClient,
+  SimpleFsStorageProvider,
+  AutojoinRoomsMixin,
+  // RustSdkCryptoStorageProvider,
 } from "matrix-bot-sdk";
-
+import addTelegramPack from "./lib/telegram/add-pack.js";
+import saveOrFindUser from "./db/save-or-find-user.js";
 
 const homeserverUrl = process.env["HOMESERVER"];
 const accessToken = process.env["ACCESS_TOKEN"];
 
 if (!homeserverUrl || !accessToken) {
-    throw new Error('Specify homeserver and access_token environment variables')
+  throw new Error("Specify homeserver and access_token environment variables");
 }
 
-// In order to make sure the bot doesn't lose its state between restarts, we'll give it a place to cache
-// any information it needs to. You can implement your own storage provider if you like, but a JSON file
-// will work fine for this example.
-const storage = new SimpleFsStorageProvider("hello-bot.json");
+const storage = new SimpleFsStorageProvider("bot-state.json");
+// const cryptoProvider = new RustSdkCryptoStorageProvider("bot-secrets");
 
-const client = new MatrixClient(homeserverUrl, accessToken, storage);
+const client = new MatrixClient(
+  homeserverUrl,
+  accessToken,
+  storage,
+  // cryptoProvider
+);
 AutojoinRoomsMixin.setupOnClient(client);
 
-client.on("room.message", async function handleCommand(roomId: string, event: any) {
+client.on(
+  "room.message",
+  async function handleCommand(roomId: string, event: any) {
     // Don't handle unhelpful events (ones that aren't text messages, are redacted, or sent by us)
-    if (event['content']?.['msgtype'] !== 'm.text') return;
-    if (event['sender'] === await client.getUserId()) return;
+    if (event["content"]?.["msgtype"] !== "m.text") return;
+    if (event["sender"] === (await client.getUserId())) return;
+
+    const userId: string = event["sender"];
 
     // Check to ensure that the `!hello` command is being run
-    const body = event['content']['body'];
-    if (!body?.startsWith("!hello")) return;
+    const body = event["content"]["body"];
+    if (!body) return;
+
+    if (body.startsWith("!hello")) {
+      await client.replyNotice(roomId, event, "Hello world!");
+      return;
+    }
+
+    if (body.startsWith("!tg https://t.me/addstickers/")) {
+      await client.setTyping(roomId, true, 30000)
+
+      let user;
+      try {
+        user = await saveOrFindUser(userId, client);
+      } catch (error) {
+        console.log(error);
+        await client.replyNotice(roomId, event, "Something went wrong :c");
+        return;
+      }
+
+      try {
+        await addTelegramPack(user, body.trim().split("/addstickers/")[1]);
+      } catch (error) {
+        console.log(error);
+        await client.replyNotice(roomId, event, "Something went wrong :c");
+      }
+
+
+        await client.replyNotice(roomId, event, "Done!");
+        await client.setTyping(roomId, false )
+    }
 
     // Now that we've passed all the checks, we can actually act upon the command
-    await client.replyNotice(roomId, event, "Hello world!");
-});
+  }
+);
 
 client.on("room.join", async (roomId: string, event: any) => {
-    // The client has joined `roomId`
-    await client.replyNotice(roomId, event, "Добро пожаловать лох");
+  // The client has joined `roomId`
+  await client.replyNotice(roomId, event, "Добро пожаловать лох");
 });
 
 export default client;
