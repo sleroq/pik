c7e9fe9539d2351b6fea2848b0475662716a1618
Sleroq <sleroq@sleroq.link>
2023-03-08 21:44:25 +0300

    Removed some useless info

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,7 +3,7 @@ import type { Component } from "solid-js";
 import styles from "./App.module.css";
 import Stickers from "./stickers";
 import { createSignal, Match, Switch } from "solid-js";
-import {userId, widgetApi} from "./connect-widget";
+import { widgetApi } from "./connect-widget";
 
 const Loading: Component = () => {
   return <div class="loading">Connecting</div>;
@@ -20,18 +20,15 @@ const App: Component = () => {
 
   return (
     <div class={styles.App}>
-      <header class={styles.header}>Pik sticker picker</header>
-
-        {userId.match(/@.*:/gm)?.[0].slice(1, -1)}'s stickers:
       <Switch
         fallback={
           "Failed to connect to matrix client. Try to restart the widget."
         }
       >
-        <Match when={widgetIsReady() === "yes"}>
+        <Match when={widgetIsReady() === "yes"} keyed>
           <Stickers />
         </Match>
-        <Match when={widgetIsReady() === "not yet"}>
+        <Match when={widgetIsReady() === "not yet"} keyed>
           <Loading />
         </Match>
       </Switch>
