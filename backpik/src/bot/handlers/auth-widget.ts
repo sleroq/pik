408574cce613f0548152f71344c158fd94f3448d
Sleroq <sleroq@sleroq.link>
2023-03-11 14:49:45 +0300

    Added auth-widget handler

---
+++ backpik/src/bot/handlers/auth-widget.ts
@@ -0,0 +1,36 @@
+import saveOrFindUser from "../../db/save-or-find-user.js";
+import client from "../bot.js";
+import Werror from "../../lib/error.js";
+
+export default async function authWidget(
+  roomId: string,
+  userId: string,
+  event: string,
+  body: string
+) {
+  const token = body.split(" ")[2];
+  if (!token) {
+    await client.replyNotice(roomId, event, "You have to specify token");
+    return;
+  }
+
+  let user;
+  try {
+    user = await saveOrFindUser(userId, client);
+  } catch (error) {
+    throw new Werror(error, "getting/saving user");
+  }
+
+  user.tokens.push({
+    token: token,
+    date: new Date(),
+  });
+
+  while (user.tokens.length > 20) {
+    user.tokens.shift()
+  }
+
+  await user.save();
+
+  await client.replyNotice(roomId, event, "Success!");
+}
