ec0f96dd37d430607ff35809e80f74e4ee478809
Sleroq <sleroq@sleroq.link>
2023-03-11 14:50:34 +0300

    Prettier

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,20 +2,20 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig(() => {
-  const apiUrl = 'https://pik.sleroq.link/';
+  const apiUrl = "https://pik.sleroq.link/";
 
   return {
     plugins: [solidPlugin()],
     base: "",
     server: {
       port: 3000,
-      cors: false
+      cors: false,
     },
     build: {
       target: "esnext",
     },
     define: {
-      'env.API_URL': `"${apiUrl}"`,
+      "env.API_URL": `"${apiUrl}"`,
     },
-  }
+  };
 });
